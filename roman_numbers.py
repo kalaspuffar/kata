926dc6ae66a3f0700bab0ece8fdf3564e19765fd
Daniel Persson <mailto.woden@gmail.com>
2016-03-29 19:52:46 +0200

    Print `4` when presented with `IV`

--- roman_numbers.py
+++ roman_numbers.py
@@ -9,15 +9,23 @@ def roman_numbers(nbr_str):
     1
     >>> roman_numbers("II")
     2
+    >>> roman_numbers("IV")
+    4
     >>> roman_numbers("V")
     5
     >>> roman_numbers("VI")
     6
     """
-    val = 0
+    value = 0
     for n in list(nbr_str):
+        next_value = 0
         if n == "I":
-            val += 1
+            next_value = 1
         elif n == "V":
-            val += 5
-    print(val)
+            next_value = 5
+
+        if next_value > value:
+            value = next_value - value
+        else:
+            value += next_value
+    print(value)
