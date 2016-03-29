adceecc3568fde956ad80e10d20573a2e7e04332
Daniel Persson <mailto.woden@gmail.com>
2016-03-29 19:43:32 +0200

    Print `5` when presented with `V`

--- roman_numbers.py
+++ roman_numbers.py
@@ -7,6 +7,10 @@ def roman_numbers(nbr_str):
     Examples:
     >>> roman_numbers("I")
     1
+    >>> roman_numbers("V")
+    5
     """
     if nbr_str == "I":
         print(1)
+    elif nbr_str == "V":
+        print(5)
