1c675a2a4d5a9e6fddb21651858a871b9ed9b10c
Daniel Persson <mailto.woden@gmail.com>
2016-03-29 20:13:15 +0200

    Print `100` when presented with `C`

--- roman_numbers.py
+++ roman_numbers.py
@@ -23,10 +23,12 @@ def roman_numbers(nbr_str):
     15
     >>> roman_numbers("L")
     50
+    >>> roman_numbers("C")
+    100
     """
     value = 0
-    roman_numbers = ["I", "V", "X", "L"]
-    decimal_numbers = [1, 5, 10, 50]
+    roman_numbers = ["I", "V", "X", "L", "C"]
+    decimal_numbers = [1, 5, 10, 50, 100]
     for n in list(nbr_str):
         next_value = 0
         for idx, val in enumerate(roman_numbers):
