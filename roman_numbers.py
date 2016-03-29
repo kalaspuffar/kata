9b407fe23902bc9c7a070bd4f75ecff970d9599d
Daniel Persson <mailto.woden@gmail.com>
2016-03-29 19:41:28 +0200

    Return  when presented with

--- roman_numbers.py
+++ roman_numbers.py
@@ -3,3 +3,10 @@ This kata will try to parse roman numbers in small increments using python
 and doctest.
 """
 def roman_numbers(nbr_str):
+    """
+    Examples:
+    >>> roman_numbers("I")
+    1
+    """
+    if nbr_str == "I":
+        print(1)
