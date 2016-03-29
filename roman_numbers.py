5cac9307f6a8d0a1ff73a24a1bbd85e8bf528f7f
Daniel Persson <mailto.woden@gmail.com>
2016-03-29 20:16:06 +0200

    Print 500, 1000 when presented with D and M

--- roman_numbers.py
+++ roman_numbers.py
@@ -25,10 +25,14 @@ def roman_numbers(nbr_str):
     50
     >>> roman_numbers("C")
     100
+    >>> roman_numbers("D")
+    500
+    >>> roman_numbers("M")
+    1000
     """
     value = 0
-    roman_numbers = ["I", "V", "X", "L", "C"]
-    decimal_numbers = [1, 5, 10, 50, 100]
+    roman_numbers = ["I", "V", "X", "L", "C", "D", "M"]
+    decimal_numbers = [1, 5, 10, 50, 100, 500, 1000]
     for n in list(nbr_str):
         next_value = 0
         for idx, val in enumerate(roman_numbers):
