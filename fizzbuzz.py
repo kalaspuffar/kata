c673e6dd4e91f5a6f03f8b8b473f50767701f652
Daniel Persson <mailto.woden@gmail.com>
2016-04-05 22:13:20 +0200

    We expect that `1` should yeld the result `1`.

---
+++ fizzbuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Givet en siffra skall man returnera siffra.
+* Om inte siffran är en trea för då returnerar man fizz
+* Om inte siffran är en fema för då returnerar man buzz
+* Om inte siffran är delbart med tre för då returnerar man fizz
+* Om inte siffran är delbart med fem för då returnerar man buzz
+* Om inte siffran är delbart med tre och fem för då returnerar
+  man fizz buzz
+* Om inte siffran innehåller siffran tre för då returnerar man fizz
+* Om inte siffran innehåller siffran fem för då returnerar man buzz
+* Om inte siffran innehåller både tre och fem för då returnerar
+  man fizz buzz
+"""
+def fizzbuzz(number):
+    """
+    >>> fizzbuzz(1)
+    1
+    """
+    pass
